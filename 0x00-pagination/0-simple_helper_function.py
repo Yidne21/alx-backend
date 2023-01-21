f96e6a4fef201461ac3c1669b3f140276fb58ef1
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-01-21 14:46:36 +0300

    add type annotation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 '''Task-0's module
 '''
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     startIndex = (page - 1) * page_size
     endIndex = page * page_size
     return (startIndex, endIndex)
